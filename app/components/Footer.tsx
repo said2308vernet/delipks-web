5e05424c9814af4743cf9d5c2e1edbc06b23fd28
said2308vernet <said2308@gmail.com>
2026-06-25 01:05:13 -0600

    Actualizar enlace de Facebook al nuevo perfil

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -94,7 +94,7 @@ export default function Footer() {
                 </svg>
               </a>
               <a
-                href="https://www.facebook.com/profile.php?id=61590947618913"
+                href="https://www.facebook.com/profile.php?id=61591171984937"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Facebook"
